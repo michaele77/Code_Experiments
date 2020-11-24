32ee5a70057b280339ce5d0706e90ba4014eec6a
Michael Ershov <m.ershov@berkeley.edu>
2020-11-24 15:55:35 -0800

    Added a nice README detailing included files

---
+++ README.md
@@ -0,0 +1,35 @@
+# Code Experiments 
+[![Build Status]
+[![Windows Build Status]
+[![js-standard-style]
+
+This is a place where I experiment with different packages, new modules, or start the prototyping for new project ideas. Most of this repository is written in Python for speed of development, code readability, and the large availability of modules to simplify implementation.
+
+# API Keys
+
+The API Keys for the different APIs I use in the code experiments are stored in a config.py file that I import for any file that requires API keys. 
+Config.py includes a function names "access_keys" that takes in the file name string, and returns the needed keys as strings. 
+Config.py is not tracked by git for security.
+
+# Files Included
+
+## book_hound.py
+An attempt to create a book recommendation engine using the goodreads API by creating a  book reviewer graph that would be traversed to find commonly agreed-upon books that are most pointed-to by the web.
+Unfortunately, goodreads API only provides limited reviewer information and limited information about users; it also has a 1 result / second access limitation.
+
+### To Dos
+Use a web-scraper instead of the API to construct the reviewer graph. The information would be stored in a database that could be used for future queries. 
+
+## craveit.py
+Experimentation with a food-ranking app that searches nearby restaurants' menus for specific food items and displays them with actual images taken from review posts.
+The yelp API not only provides limited endpoints to access images (only gives a maximum of 3 images), but it also provides zero menu information.
+
+### To Dos 
+Use a web-scraper to search for nearby restaurants based on location. A combination of the API (for getting restaurant names) and the web-scraper (for getting the menu/associated images) should be used.
+
+## webscraper_PS5
+An attempt to scrap commonly used websites for PS5 sales (Walmart, Best-buy, etc) and build a rough notifications program to text users once the PS5 is in stock.
+Existing notification sites have a high latency (ie updating once a minute) which is often too slow for flash sales.
+
+### To Dos 
+None
--- craveit.py
+++ craveit.py
@@ -37,22 +37,184 @@ except ImportError as import_err:
 # print(content.decode())
 
 
-#Example 2: try making a get request from the yelp API
+# #Example 2: try making a get request from the yelp API
+#
+# http = httplib2.Http()
+#
+# API_host = 'https://api.yelp.com'
+# search_path = '/v3/businesses/search'
+# yelp_url = API_host + search_path
+#
+#
+# content = http.request(yelp_url, method="GET")[1]
+#
+#
+# auth = requests.auth.HTTPBasicAuth('clientid', CLIENT_ID)
+# r = requests.get(yelp_url, auth=auth)
+# print(r.status_code)
+# print(r.json())
 
-http = httplib2.Http()
-
-API_host = 'https://api.yelp.com'
-search_path = '/v3/businesses/search'
-yelp_url = API_host + search_path
 
 
-content = http.request(yelp_url, method="GET")[1]
+
+####Below is from the yelp fusion API code examples file:
+####Source: https://github.com/Yelp/yelp-fusion/blob/master/fusion/python/sample.py
 
 
-auth = requests.auth.HTTPBasicAuth('clientid', CLIENT_ID)
-r = requests.get(yelp_url, auth=auth)
-print(r.status_code)
-print(r.json())
-print('update')
+import argparse
+import json
+import pprint
+import requests
+import sys
+import urllib
+
+
+# This client code can run on Python 2.x or 3.x.  Your imports can be
+# simpler if you only need one of those.
+try:
+    # For Python 3.0 and later
+    from urllib.error import HTTPError
+    from urllib.parse import quote
+    from urllib.parse import urlencode
+except ImportError:
+    # Fall back to Python 2's urllib2 and urllib
+    from urllib2 import HTTPError
+    from urllib import quote
+    from urllib import urlencode
+
+
+# Yelp Fusion no longer uses OAuth as of December 7, 2017.
+# You no longer need to provide Client ID to fetch Data
+# It now uses private keys to authenticate requests (API Key)
+# You can find it on
+# https://www.yelp.com/developers/v3/manage_app
+# API_KEY= None
+
+
+# API constants, you shouldn't have to change these.
+API_HOST = 'https://api.yelp.com'
+SEARCH_PATH = '/v3/businesses/search'
+BUSINESS_PATH = '/v3/businesses/'  # Business ID will come after slash.
+
+
+# Defaults for our simple example.
+DEFAULT_TERM = 'dinner'
+DEFAULT_LOCATION = 'San Francisco, CA'
+SEARCH_LIMIT = 3
+
+
+
+
+
+
+
+
+
+
+def request(host, path, api_key, url_params=None):
+    """Given your API_KEY, send a GET request to the API.
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        API_KEY (str): Your API Key.
+        url_params (dict): An optional set of query parameters in the request.
+    Returns:
+        dict: The JSON response from the request.
+    Raises:
+        HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = '{0}{1}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer %s' % api_key,
+    }
+
+    print(u'Querying {0} ...'.format(url))
+
+    response = requests.request('GET', url, headers=headers, params=url_params)
+
+    return response.json()
+
+
+def search(api_key, term, location):
+    """Query the Search API by a search term and location.
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+    Returns:
+        dict: The JSON response from the request.
+    """
+
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, api_key, url_params=url_params)
+
+
+def get_business(api_key, business_id):
+    """Query the Business API by a business ID.
+    Args:
+        business_id (str): The ID of the business to query.
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return request(API_HOST, business_path, api_key)
+
+
+def query_api(term, location):
+    """Queries the API by the input values from the user.
+    Args:
+        term (str): The search term to query.
+        location (str): The location of the business to query.
+    """
+    #Returns a json of the GET request from the URL
+    response = search(API_KEY, term, location)
+
+    businesses = response.get('businesses')
+
+    if not businesses:
+        print(u'No businesses for {0} in {1} found.'.format(term, location))
+        return
+
+    business_id = businesses[0]['id']
+
+    print(u'{0} businesses found, querying business info ' \
+        'for the top result "{1}" ...'.format(
+            len(businesses), business_id))
+    response = get_business(API_KEY, business_id)
+
+    print(u'Result for business "{0}" found:'.format(business_id))
+    pprint.pprint(response, indent=2)
+
+
+
+
+
+
+#TODO: make input search terms and location user defined/input!
+search_term = DEFAULT_TERM
+search_loc = DEFAULT_LOCATION
+
+#Query structure:
+#query_api --> search ----> request
+#       v----> get_buisness --^
+
+try:
+    query_api(search_term, search_loc)
+except HTTPError as error:
+    sys.exit(
+        'Encountered HTTP error {0} on {1}:\n {2}\nAbort program.'.format(
+            error.code,
+            error.url,
+            error.read(),
+        )
+    )
+
+
+
 
 pass
